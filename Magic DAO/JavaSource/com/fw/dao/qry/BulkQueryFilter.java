23449501ef10ef890e1f87a5e02ef639ce6c0737
akranthikiran <akranthikiran@gmail.com>
2015-05-02 19:02:32 +0530

    Removed Magic DAO and added CCG binary

--- Magic DAO/JavaSource/com/fw/dao/qry/BulkQueryFilter.java
+++
@@ -1,8 +0,0 @@
-package com.fw.dao.qry;
-
-public interface BulkQueryFilter extends QueryFilter
-{
-	public Class<?> getParamType(String paramName);
-	public boolean next();
-	public int getCommitCount();
-}
