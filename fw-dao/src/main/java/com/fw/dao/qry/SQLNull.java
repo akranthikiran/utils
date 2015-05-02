4f0e5f4b2b0e31ed90ceec773046b2db4ae6ae60
akranthikiran <akranthikiran@gmail.com>
2015-05-02 19:40:19 +0530

    Adding fw-dao to GIT

---
+++ fw-dao/src/main/java/com/fw/dao/qry/SQLNull.java
@@ -0,0 +1,21 @@
+package com.fw.dao.qry;
+
+class SQLNull
+{
+	private int type;
+
+		public SQLNull(int type)
+	    {
+		    this.type=type;
+	    }
+
+		public int getType()
+        {
+        	return type;
+        }
+		
+		public String toString()
+		{
+			return "NULL";
+		}
+}
